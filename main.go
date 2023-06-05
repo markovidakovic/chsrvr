beaf9b084071d6282031d59d4dc6056864741b3a
Marko Vidaković <markovidakovic29@gmail.com>
2023-06-05 18:30:28 +0200

    -

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/websocket"
 )
@@ -15,5 +16,59 @@ type Message struct {
 }
 
 func main() {
-	fmt.Println("hello")
+	http.HandleFunc("/ws", handleConnections)
+
+	// start listening for incoming messages
+	go handleMessages()
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("Error starting the server: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// upgrade the GET request to websocket
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		log.Fatal("Error upgrading the connection to websocket: ", err)
+	}
+
+	// register new client
+	clients[ws] = true
+	log.Println("Client connected")
+
+	// close the connection when this func returns
+	defer func() {
+		delete(clients, ws)
+		ws.Close()
+	}()
+
+	for {
+		var msg Message
+		// read message from websocket
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("Error reading the message: %v", err)
+			break
+		}
+		// send the received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// get the next message from the channel
+		msg := <-broadcast
+		// send message to all connected clients
+		for k := range clients {
+			err := k.WriteJSON(msg)
+			if err != nil {
+				log.Printf("Error writing message: %v", err)
+				k.Close()
+				delete(clients, k)
+			}
+		}
+	}
 }
